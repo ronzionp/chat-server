b9292d1de00a9fb0f5bdcf6e6303af71615addf4
Ron Zionpour <ron@innovid.com>
2015-02-02 22:03:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var uuidGen = require('uuid');
+var users = {};
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+
+
+io.on('connection', function(socket){
+
+  io.on('disconnect', function(){
+    console.log('user disconnected'); //TODO: remove from client's connected users
+  });
+
+  socket.on('join', function(user){
+    io.emit('user connected', user.name);
+    // Generate a v4 (random) id
+    var uuid = uuidGen.v4(); // -> '110ec58a-a0f2-4ac4-8393-c866d813b8d1'
+    users[uuid] = user.name;
+    //socket.emit('my user details', {id: uuid, name: user.name});
+  });
+
+  socket.on('chat message', function(userMsg){
+    io.emit('chat message', userMsg);
+  });
+
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
