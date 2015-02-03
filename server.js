070118e1c39002ea5335aff437d363b8f0f81248
Ron Zionpour <ron@innovid.com>
2015-02-03 22:14:48 +0200

    server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var uuidGen = require('uuid');
 var users = {};
+var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -36,6 +37,6 @@ io.on('connection', function(socket){
 
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
